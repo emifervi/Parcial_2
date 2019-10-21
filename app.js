491b7d9dde869bed4251dd55bf1191ae1aa41bbf
emifervi <emifervi@hotmail.com>
2019-10-21 17:09:35 -0500

    Some error handling

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 3000
 const app = express();
 const metObjects = `https://collectionapi.metmuseum.org/public/collection/v1/search`
 const metObject = `https://collectionapi.metmuseum.org/public/collection/v1/objects/`
+
 // id part
 app.get('/students:id', function(req, res) {
-    if(req.params.id === ':A00959385' || req.params.id === 'A00959385'){
+    
+  if(req.params.id === ':A00959385' || req.params.id === 'A00959385' || req.params.id === '/A00959385'){
       return res.status(200).json({
         'id': 'A00959385',
         'fullname': 'Emilio Fernando Alonso Villa',
@@ -16,8 +18,9 @@ app.get('/students:id', function(req, res) {
         'age': '24'
       });
     } else {
-      console.log(req.params.id)
-      throw new Error('id no válido');
+      throw Error(JSON.stringify({
+        "ErrorType": "error desconocido"
+      }));
     }
 });
 
@@ -25,12 +28,29 @@ app.get('/students:id', function(req, res) {
 app.get('/met', function(req, res) {
   const metSearch = `${metObjects}?q=${req.query.search}`
   request.get({url:metSearch, json:true}, (error, response, body) => {
-
+    console.log(body);
+    console.log(response.statusCode);
+    if(error) {
+      const errorMsg = error.errno === 'ENOTFOUND' ? 'No hay internet' : 'Error desconocido';
+      res.send({
+        'Error type': errorMsg
+      });
+    }
+    if(body.total === 0) {
+      res.send({
+        'Error type': 'No se encontraron objetos correspondientes a la busqueda'
+      });
+    }
     const artId = body.objectIDs[0];
     const artInfo = `${metObject}${artId}`
 
     request.get({url:artInfo, json:true}, (error, response, body) => {
-      console.log(body);
+      if(error) {
+        const errorMsg = error.errno === 'ENOTFOUND' ? 'No hay internet' : 'Error desconocido';
+        res.send({
+          'Error type': errorMsg
+        });
+      }
       return res.status(200).json({
         'searchTerm': req.query.search,
         'artist': body.constituents[0].name,
@@ -38,7 +58,7 @@ app.get('/met', function(req, res) {
         'year': body.objectEndDate,
         'technique': body.medium,
         'metUrl': body.objectURL
-      })
+      });
     })
   });
 });
